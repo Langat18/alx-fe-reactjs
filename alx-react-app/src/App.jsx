3a0a136ad33c8d3a8fa2293e02550ceb7bcfe9b3
Langat18 <langatclement18@gmail.com>
2025-07-21 01:12:46 +0300

    props and profile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,4 @@
+import UserProfile from './components/UserProfile'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
@@ -37,7 +38,9 @@ function App() {
       <WelcomeMessage />
       <Header />
       <MainContent />
+       <UserProfile name="Kipkirui" age="25" bio="Chess and coding" />
       <Footer />
+     
     </>
   )
 }
