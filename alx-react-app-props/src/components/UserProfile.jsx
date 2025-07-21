dc27c25bd7e9256e52d45620a1b0dde434a5a1df
Langat18 <langatclement18@gmail.com>
2025-07-21 10:05:52 +0300

    Usercontext

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,7 +1,16 @@
-import UserInfo from './UserInfo';
+import { useContext } from 'react';
+import UserContext from '../UserContext';
 
-function ProfilePage() {
-  return <UserInfo />;
-}
+const UserProfile = () => {
+  const userData = useContext(UserContext);
 
-export default ProfilePage;
+  return (
+    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
+      <h2 style={{ color: 'blue' }}>{userData.name}</h2>
+      <p>Age: <span style={{ fontWeight: 'bold' }}>{userData.age}</span></p>
+      <p>Bio: {userData.bio}</p>
+    </div>
+  );
+};
+
+export default UserProfile;
