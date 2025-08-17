9ca9cac3c33f0114495a235c2ba1a1f65670147a
Langat18 <langatclement18@gmail.com>
2025-08-17 14:17:57 +0300

    Changed the profile picture to my github profile and changed the page title from default

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,7 +1,7 @@
 function UserProfile() {
   return (
     <div className="user-profile bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
-      <img className="rounded-full w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out cursor-pointer" src="https://via.placeholder.com/150" alt="User" />
+      <img className="rounded-full w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out cursor-pointer" src="https://via.https://avatars.githubusercontent.com/u/57733037?s=400&u=292aa6e394bf88a448af925cc56d08b1272c8daa&v=4placeholder.com/150" alt="User" />
       <h1 className="text-lg sm:text-lg md:text-xl text-blue-800 hover:text-blue-500 my-4 text-center transition-colors duration-300 ease-in-out cursor-pointer ">John Doe</h1>
       <p className="text-sm sm:text-sm md:text-base text-gray-600 text-center">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
