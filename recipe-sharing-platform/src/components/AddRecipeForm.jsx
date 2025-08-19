34c9e42a503d6fa4ef45d0da33050caaca82eb3f
Langat18 <langatclement18@gmail.com>
2025-08-19 12:13:49 +0300

    Handled the error of etargets in AddRecipeForm.jsx

--- recipe-sharing-platform/src/components/AddRecipeForm.jsx
+++ recipe-sharing-platform/src/components/AddRecipeForm.jsx
@@ -30,7 +30,8 @@ function AddRecipeForm() {
   };
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
+    const name = e.target.name;
+    const value = e.target.value;
     setFormData(prev => ({ ...prev, [name]: value }));
     setErrors(prev => ({ ...prev, [name]: '' }));
   };
